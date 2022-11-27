7dc7766dfbfea61c14c4ecb1207bafa480c993ff
Amit <amitpr7828@gmail.com>
2022-11-27 13:47:55 +0530

    made responsive input component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./Style.scss"
-import Register from "./Pages/Register";
-import Login from "./Pages/Login"
+// import Register from "./Pages/Register";
+// import Login from "./Pages/Login"
 import Home from "./Pages/Home"
 
 function App() {
