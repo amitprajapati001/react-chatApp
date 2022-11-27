5ca8a710ff5953d2ae43d2ac89b6849dfca479af
Amit <amitpr7828@gmail.com>
2022-11-27 13:44:24 +0530

    made responsive

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -9,7 +9,7 @@ export default function Message() {
       </div>
       <div className="message-content">
         <p>Hello</p>
-        {/* <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" /> */}
+        <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
       </div>
       
     </div>
